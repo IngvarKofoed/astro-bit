dbcbd0d8a14fc55fd9a9b487c70181fa69e4db11
Martin Jensen <ingvar@netaben.dk>
2018-09-07 14:30:16 +0200

    ToDegrees added as an extension for Longitude instances.

---
+++ AstroBit.Test/Math/LongitudeExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using AstroBit.Math;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AstroBit.Test.Math
+{
+    public static class LongitudeExtensionsTest
+    {
+        [TestClass]
+        public class ToDegrees
+        {
+            [DataTestMethod]
+            [DataRow(50, 0, 0, LongitudeDirection.East, 50.0)]
+            [DataRow(50, 0, 0, LongitudeDirection.West, -50.0)]
+            [DataRow(50, 30, 12, LongitudeDirection.East, 50.50333333333333)]
+            [DataRow(50, 30, 12, LongitudeDirection.West, -50.50333333333333)]
+            public void ShouldMakeCorrectConvertions(int degrees, int minutes, double seconds, LongitudeDirection direction, double expectedDegrees)
+            {
+                var longitudeDegrees = new Longitude(50, minutes, seconds, direction).ToDegrees();
+                longitudeDegrees.Should().Be(expectedDegrees);
+            }
+        }
+    }
+}
---
+++ astro-bit/Math/LongitudeExtensions.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable SA1407 // Arithmetic expressions must declare precedence
+
+namespace AstroBit.Math
+{
+    /// <summary>
+    /// Extension methods for <see cref="Longitude"/>.
+    /// </summary>
+    public static class LongitudeExtensions
+    {
+        /// <summary>
+        /// Converts the given <paramref name="longitude"/> to degrees in the range of [-180, 180]
+        /// where 0 is the prime meridian.
+        /// </summary>
+        /// <param name="longitude">The longitude to convert.</param>
+        /// <returns>Returns the longitude in degrees.</returns>
+        public static double ToDegrees(this Longitude longitude) =>
+            longitude.GetDirectionFactor() * (longitude.Degrees + longitude.Minutes / 60.0 + longitude.Seconds / 3600.0);
+
+        private static int GetDirectionFactor(this Longitude longitude) =>
+            longitude.Direction == LongitudeDirection.East ? 1 : -1;
+    }
+}
