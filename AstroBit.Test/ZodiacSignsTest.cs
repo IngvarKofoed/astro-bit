fe0e4e0111d37484a83937d4b9ee583bcaff7b20
Martin Jensen <ingvar@netaben.dk>
2018-09-07 15:20:42 +0200

    Adding method for getting the unicode zodiac sign.

---
+++ AstroBit.Test/ZodiacSignsTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AstroBit.Test
+{
+    public static class ZodiacSignsTest
+    {
+        [TestClass]
+        public class GetByIndex
+        {
+            [DataTestMethod]
+            [DataRow(0, "♈")]
+            [DataRow(7, "♏")]
+            [DataRow(11, "♓")]
+            public void ShouldReturnExpectedSign(int index, string expectedSign)
+            {
+                ZodiacSigns.GetByIndex(index).Should().Be(expectedSign);
+            }
+
+            [DataTestMethod]
+            [DataRow(-1, "")]
+            [DataRow(12, "")]
+            public void ShouldThrowWhenOutOfRange(int index, string expectedSign)
+            {
+                Action shouldThrow = () => ZodiacSigns.GetByIndex(index);
+                shouldThrow.Should().Throw<ArgumentException>();
+            }
+        }
+    }
+}
