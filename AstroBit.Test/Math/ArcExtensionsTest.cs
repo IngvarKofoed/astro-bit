ff752acdd69f092125c1d1e4a25413ce21b4c48d
Martin Jensen <ingvar@netaben.dk>
2018-09-07 12:37:15 +0200

    Adding a ToTime returning TimeSpan to the Arc type.

---
+++ AstroBit.Test/Math/ArcExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AstroBit.Math;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace AstroBit.Test.Math
+{
+    public static class ArcExtensionsTest
+    {
+        [TestClass]
+        public class ToTime
+        {
+            [TestMethod]
+            public void ShouldReturnCorrectTime()
+            {
+                var time1 = new Arc(90, 0, 0).ToTime();
+                time1.Should().Be(TimeSpan.FromHours(6));
+
+                var time2 = new Arc(180, 30, 0).ToTime();
+                time2.Should().Be(new TimeSpan(12, 2, 0));
+
+                var time3 = new Arc(200, 20, 20).ToTime();
+                time3.Should().Be(new TimeSpan(13, 21, 21));
+            }
+        }
+    }
+}
---
+++ astro-bit/Math/ArcExtensions.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable SA1407 // Arithmetic expressions must declare precedence
+
+using System;
+
+namespace AstroBit.Math
+{
+    /// <summary>
+    /// Extension methods for <see cref="Arc"/>.
+    /// </summary>
+    public static class ArcExtensions
+    {
+        /// <summary>
+        /// Converts the given <paramref name="arc"/> into a <see cref="TimeSpan"/>
+        /// where there are 24 hours on a full circle.
+        /// </summary>
+        /// <param name="arc">The arc to convert.</param>
+        /// <returns>Returns the time for the given arc where 360 degrees is 24 hours.</returns>
+        public static TimeSpan ToTime(this Arc arc)
+        {
+            int hours = arc.Degrees / 15;
+            int minutes = (arc.Degrees % 15) * 4 + (arc.Minutes / 15);
+            int seconds = (arc.Minutes % 15) * 4 + (int)(arc.Seconds / 15);
+
+            return TimeSpan.FromSeconds(hours * 3600 + minutes * 60 + seconds);
+        }
+    }
+}
