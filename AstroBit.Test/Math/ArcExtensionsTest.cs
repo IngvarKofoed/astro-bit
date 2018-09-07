39e1fbf3c9f8a2ee094d02dffefcdbbc5e72364d
Martin Jensen <ingvar@netaben.dk>
2018-09-07 13:52:18 +0200

    Fixing test after Arc is now wrapping and truncating.

--- AstroBit.Test/Math/ArcExtensionsTest.cs
+++ AstroBit.Test/Math/ArcExtensionsTest.cs
@@ -15,7 +15,6 @@ namespace AstroBit.Test.Math
             [DataRow("6:00:00", 90, 0, 0)]
             [DataRow("12:02:00", 180, 30, 0)]
             [DataRow("13:21:21", 200, 20, 20)]
-            [DataRow("1.06:00:00", 450, 0, 0)]
             public void ShouldReturnCorrectTime(string expectedTime, int degrees, int minutes, int seconds)
             {
                 var time = new Arc(degrees, minutes, seconds).ToTime();
