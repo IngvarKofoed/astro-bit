2705c7be691f9508e905f4a8775f7244faf34dbf
Martin Jensen <ingvar@netaben.dk>
2018-09-07 13:47:19 +0200

    Truncate for doubles added as extension on double

---
+++ AstroBit.Test/Math/NumericExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using AstroBit.Math;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AstroBit.Test.Math
+{
+    public static class NumericExtensionsTest
+    {
+        [TestClass]
+        public class Truncate
+        {
+            [DataTestMethod]
+            [DataRow(127.0, 127.0, 180, DisplayName = "Not change value when within max")]
+            [DataRow(10.0, 190.0, 180, DisplayName = "Truncate value to be less than max")]
+            [DataRow(10.0, 370.0, 180, DisplayName = "Truncate value larger than 2x max and give non-negative value and less than max")]
+            [DataRow(53.0, -127.0, 180, DisplayName = "Give non-negative value within -max")]
+            [DataRow(175.0, -185.0, 180, DisplayName = "Truncate and give non-negative value")]
+            [DataRow(175.0, -365.0, 180, DisplayName = "Truncate value less than 2x -max and give non-negative value and less than max")]
+            [DataRow(127.456, 127.456, 180, DisplayName = "Preserve decimals")]
+            public void ShouldTruncateCorrectly(double expectedValue, double givenValue, int maxValue)
+            {
+                givenValue.Truncate(maxValue).Should().Be(expectedValue);
+            }
+        }
+    }
+}
---
+++ astro-bit/Math/NumericExtensions.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable SA1407 // Arithmetic expressions must declare precedence
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AstroBit.Math
+{
+    public static class NumericExtensions
+    {
+        public static double Truncate(this double value, int maxValue) =>
+            value - (int)value / maxValue * maxValue + (value < 0.0 ? maxValue : 0);
+    }
+}
