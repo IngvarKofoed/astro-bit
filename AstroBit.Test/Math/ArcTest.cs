0cda5810c5baab2721cf93c8977c77ed900fc5ce
Martin Jensen <ingvar@netaben.dk>
2018-09-07 11:16:13 +0200

    Adding arc structure with tests

---
+++ AstroBit.Test/Math/ArcTest.cs
@@ -0,0 +1,20 @@
+﻿using AstroBit.Math;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AstroBit.Test.Math
+{
+    [TestClass]
+    public class ArcTest
+    {
+        [TestMethod]
+        public void ToStringShouldReturnCorrectFormattedString()
+        {
+            var arc1 = new Arc(1, 10, 1.12);
+            arc1.ToString().Should().Be("1°10'1\"");
+
+            var arc2 = new Arc(1, 10, 1.9);
+            arc2.ToString().Should().Be("1°10'2\"");
+        }
+    }
+}
---
+++ astro-bit/Math/Arc.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace AstroBit.Math
+{
+    /// <summary>
+    /// Represents a circle arc in degrees, minutes and seconds.
+    /// </summary>
+    [DebuggerDisplay("{ToString()}")]
+    public struct Arc
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Arc"/> struct.
+        /// </summary>
+        /// <param name="degrees">The degrees of the arc.</param>
+        /// <param name="minutes">The minutes of the arc.</param>
+        /// <param name="seconds">The seconds of the arc.</param>
+        public Arc(int degrees, int minutes, double seconds)
+        {
+            Degrees = degrees;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        /// <summary>
+        /// Gets the degrees of the arc.
+        /// </summary>
+        public int Degrees { get; }
+
+        /// <summary>
+        /// Gets the minutes of the arc.
+        /// </summary>
+        public int Minutes { get; }
+
+        /// <summary>
+        /// Gets the seconds of the arc.
+        /// </summary>
+        public double Seconds { get; }
+
+        public override string ToString() =>
+            $"{Degrees}°{Minutes}'{Seconds:F0}\"";
+    }
+}
