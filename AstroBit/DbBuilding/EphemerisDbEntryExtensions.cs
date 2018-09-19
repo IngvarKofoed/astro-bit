3a3e6f2d58a681144552a289650ff3749072ac21
Martin Jensen <ingvar@netaben.dk>
2018-09-19 10:17:02 +0200

    Renaming to match the renaming of EphemerisDbEntry

--- AstroBit/DbBuilding/EphemerisDbEntryExtensions.cs
+++ AstroBit/DbBuilding/EphemerisDbEntryExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace AstroBit.DbBuilding
 {
-    public static class EphemerisEntryExtensions
+    public static class EphemerisDbEntryExtensions
     {
         public static long ToId(this DateTime dateTime) =>
             (long)dateTime.Hour << 8 + dateTime.Minute;
