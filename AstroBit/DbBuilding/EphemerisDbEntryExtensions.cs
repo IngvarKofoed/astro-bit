4bb473570eddda507c2277f20060c94a9c21fd2c
Martin Jensen <ingvar@netaben.dk>
2018-09-19 10:19:54 +0200

    Adding extension for converting EphemerisDbEntry to EphemerisEntry

--- AstroBit/DbBuilding/EphemerisDbEntryExtensions.cs
+++ AstroBit/DbBuilding/EphemerisDbEntryExtensions.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using AstroBit.Ephemeris;
 
 namespace AstroBit.DbBuilding
 {
     public static class EphemerisDbEntryExtensions
     {
         public static long ToId(this DateTime dateTime) =>
-            (long)dateTime.Hour << 8 + dateTime.Minute;
+            (long)dateTime.Hour << (8 + dateTime.Minute);
+
+        public static EphemerisEntry ToEphemerisEntry(this EphemerisDbEntry entry) =>
+            new EphemerisEntry(
+                entry.Date,
+                entry.SiderealTime,
+                entry.Sun,
+                entry.Moon,
+                entry.Mercury,
+                entry.MercuryRetrograde,
+                entry.Venus,
+                entry.VenusRetrograde,
+                entry.Mars,
+                entry.MarsRetrograde,
+                entry.Jupiter,
+                entry.JupiterRetrograde,
+                entry.Saturn,
+                entry.SaturnRetrograde,
+                entry.Uranus,
+                entry.UranusRetrograde,
+                entry.Neptune,
+                entry.NeptuneRetrograde,
+                entry.Pluto,
+                entry.PlutoRetrograde,
+                entry.Chiron,
+                entry.NorthNode,
+                entry.BlackMoonLilith);
     }
 }
