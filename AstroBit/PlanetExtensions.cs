424c9215dc597bf292e29b64cb0d6aba9fc987e2
Martin Jensen <ingvar@netaben.dk>
2018-09-25 13:55:14 +0200

    Adding GetZodiacLocalDegrees

--- AstroBit/PlanetExtensions.cs
+++ AstroBit/PlanetExtensions.cs
@@ -2,10 +2,13 @@
 {
     public static class PlanetExtensions
     {
-        public static double GetZodiacStartDegree(this Planet planet) =>
-            ZodiacExtensions.GetZodiacLocalStartDegrees(planet.AbsolutePosition);
-
         public static Zodiac GetZodiac(this Planet planet) =>
             ZodiacExtensions.GetZodiac(planet.AbsolutePosition);
+
+        public static double GetZodiacLocalDegrees(this Planet planet) =>
+            ZodiacExtensions.GetZodiacLocalDegrees(planet.AbsolutePosition);
+
+        public static double GetZodiacStartDegree(this Planet planet) =>
+            ZodiacExtensions.GetZodiacLocalStartDegrees(planet.AbsolutePosition);
     }
 }
