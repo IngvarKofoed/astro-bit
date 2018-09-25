9da9f7c2850ea49faa7b4c7c6c9b0e719ecfbdd1
Martin Jensen <ingvar@netaben.dk>
2018-09-25 13:55:52 +0200

    Adding EfEphemrisEntity to EphemerisEntity

---
+++ AstroBit/Database/EfEphemerisEntryExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace AstroBit.Database
+{
+    public static class EfEphemerisEntryExtensions
+    {
+        public static EphemerisEntry ToEntry(this EfEphemerisEntry entry) =>
+            new EphemerisEntry(
+                entry.Date,
+                entry.SiderealTime,
+                new Planet(PlanetType.Sun, entry.Sun, PlanetDirection.Direct),
+                new Planet(PlanetType.Moon, entry.Moon, PlanetDirection.Direct),
+                new Planet(PlanetType.Mercury, entry.Mercury, entry.MercuryRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.Venus, entry.Venus, entry.VenusRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.Mars, entry.Mars, entry.MarsRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.Jupiter, entry.Jupiter, entry.JupiterRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.Saturn, entry.Saturn, entry.SaturnRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.Uranus, entry.Uranus, entry.UranusRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.Neptune, entry.Neptune, entry.NeptuneRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.Pluto, entry.Pluto, entry.PlutoRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.TrueNode, entry.TrueNode, entry.TrueNodeRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.MeanNode, entry.MeanNode, entry.MeanNodeRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.BlackMoonLilith, entry.BlackMoonLilith, entry.BlackMoonLilithRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct),
+                new Planet(PlanetType.Chiron, entry.Chiron, entry.ChironRetrograde ? PlanetDirection.Retrograde : PlanetDirection.Direct));
+    }
+}
