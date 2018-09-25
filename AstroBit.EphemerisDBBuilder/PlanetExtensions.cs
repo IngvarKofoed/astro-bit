fdfb51322c7442677d3abe9384ce63692e053eee
Martin Jensen <ingvar@netaben.dk>
2018-09-25 10:48:07 +0200

    Minor changes to planet extensions.

--- AstroBit.EphemerisDBBuilder/PlanetExtensions.cs
+++ AstroBit.EphemerisDBBuilder/PlanetExtensions.cs
@@ -2,7 +2,10 @@
 {
     public static class PlanetExtensions
     {
-        public static double GetZodiacStartDegree(this Planet planet)  =>
-            ((int)planet.AbsolutePosition / 30) * 30;
+        public static double GetZodiacStartDegree(this Planet planet) =>
+            ZodiacExtensions.GetZodiacLocalStartDegrees(planet.AbsolutePosition);
+
+        public static Zodiac GetZodiac(this Planet planet) =>
+            ZodiacExtensions.GetZodiac(planet.AbsolutePosition);
     }
 }
