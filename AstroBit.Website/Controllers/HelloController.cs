8e944786b0dff3307b0aec8077b5175fc2b0cbb4
Martin Jensen <ingvar@netaben.dk>
2018-10-07 18:20:48 +0200

    Adding website backend

---
+++ AstroBit.Website/Controllers/HelloController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Http;
+using AstroBit.AstroMath;
+using AstroBit.HumanDesign;
+
+namespace AstroBit.Website.Controllers
+{
+    public class HelloController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            //var date = new DateTime(1977, 8, 30, 5, 10, 0, DateTimeKind.Utc); // Martin
+            var date = DateTime.Now.ToUniversalTime();
+
+            var ephemeris = new Ephemeris();
+            var entry = ephemeris.Get(date);
+
+            var humanDesignGates = new HumanDesignGates();
+
+            // var sunGate = humanDesignGates.GetDefinedGate((entry.Sun.AbsolutePosition - 88.0).Truncate(360));
+            var sunGate = humanDesignGates.GetDefinedGate(entry.Sun.AbsolutePosition);
+            var earthGate = sunGate.GetOppositeGate();
+
+            yield return "Sun: " + sunGate;
+            yield return "Earth: " + earthGate;
+            yield return "Moon: " + humanDesignGates.GetDefinedGate(entry.Moon.AbsolutePosition);
+            yield return "North Node: " + humanDesignGates.GetDefinedGate(entry.TrueNode.AbsolutePosition);
+            yield return "South Node: " + humanDesignGates.GetDefinedGate((entry.TrueNode.AbsolutePosition + 180.0).Truncate(360));
+            yield return "Mercury: " + humanDesignGates.GetDefinedGate(entry.Mercury.AbsolutePosition);
+            yield return "Venus: " + humanDesignGates.GetDefinedGate(entry.Venus.AbsolutePosition);
+            yield return "Mars: " + humanDesignGates.GetDefinedGate(entry.Mars.AbsolutePosition);
+            yield return "Jupiter: " + humanDesignGates.GetDefinedGate(entry.Jupiter.AbsolutePosition);
+            yield return "Saturn: " + humanDesignGates.GetDefinedGate(entry.Saturn.AbsolutePosition);
+            yield return "Uranus: " + humanDesignGates.GetDefinedGate(entry.Uranus.AbsolutePosition);
+            yield return "Neptune: " + humanDesignGates.GetDefinedGate(entry.Neptune.AbsolutePosition);
+            yield return "Pluto: " + humanDesignGates.GetDefinedGate(entry.Pluto.AbsolutePosition);
+        }
+    }
+}
---
+++ AstroBit.Website/Controllers/PingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AstroBit.Website.Controllers
+{
+    public class PingController : ApiController
+    {
+        // GET: api/Ping
+        public IEnumerable<string> Get()
+        {
+            return Enum
+                .GetNames(typeof(Zodiac))
+                .Select(x => (Zodiac)Enum.Parse(typeof(Zodiac), x))
+                .Select(x => x.GetSign());
+        }
+
+        // GET: api/Ping/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Ping
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Ping/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Ping/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
