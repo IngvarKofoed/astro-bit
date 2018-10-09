21b3f4e4e50e3b98b9ff4d5bd0fa3cef32ea11d2
Martin Jensen <ingvar@netaben.dk>
2018-10-09 21:29:06 +0200

    Adding InkScape cleaner

---
+++ AstroBit.SvgCleaner.Console/InkScapeSvgCleaner.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace AstroBit.SvgCleaner
+{
+    public static class InkScapeSvgCleaner
+    {
+        private static readonly string[] nameOfElementsToRemove = new [] { "image", "namedview", "metadata"};
+
+        public static void Clean(XDocument document)
+        {
+            Clean(document.Root);
+        }
+
+        private static void Clean(XElement element)
+        {
+            if (nameOfElementsToRemove.Contains(element.Name.LocalName))
+            {
+                element.Remove();
+                return;
+            }
+
+            if (element.Name.LocalName == "text")
+            {
+                var child = element.Elements().Where(x => x.Name.LocalName == "tspan").FirstOrDefault();
+                if (child != null)
+                {
+                    element.Add(child.Value);
+                    child.Remove();
+                }
+            }
+
+            foreach (XAttribute attribute in element.Attributes().ToArray())
+            {
+                if (attribute.Name.NamespaceName != string.Empty || attribute.Name.LocalName == "xmlns")
+                {
+                    attribute.Remove();
+                }
+                else
+                {
+                    Clean(attribute);
+                }
+            }
+
+            var elementId = element.Attribute("id").Value;
+            if (elementId.StartsWith("gate_text_") || elementId.StartsWith("gate_background_"))
+            {
+                element.Add(new XAttribute("onClick", $"#_ => this.onGateClick({GetGateId(elementId)})#"));
+            }
+
+            foreach (var child in element.Elements().ToArray())
+            {
+                Clean(child);
+            }
+        }
+
+        private static void Clean(XAttribute attribute)
+        {
+            var elementId = attribute.Parent.Attribute("id").Value;
+
+            if (attribute.Name == "style")
+            {
+                if (elementId == "pattern_double_defined_1")
+                {
+                    attribute.Value = $"#this.designGatePathStyle#";
+                }
+                else if (elementId == "pattern_double_defined_2")
+                {
+                    attribute.Value = $"#this.personalityGatePathStyle#";
+                }
+                else if (elementId.StartsWith("gate_text_"))
+                {
+                    attribute.Value = $"#this.getGateTextStyle({GetGateId(elementId)})#";
+                }
+                else if (elementId.StartsWith("gate_background_"))
+                {
+                    attribute.Value = $"#this.getGateBackgroundStyle({GetGateId(elementId)})#";
+                }
+                else if (elementId.StartsWith("gatepath_"))
+                {
+                    attribute.Value = $"#this.getGatePathStyle({GetGateId(elementId)})#";
+                }
+                else 
+                {
+                    attribute.Value = "#this.basicStyle#";
+                }                
+            }
+        }
+
+
+        private static int GetGateId(string idString) =>
+            int.Parse(idString.Split('_').Last());
+    }
+}
---
+++ AstroBit.SvgCleaner.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace AstroBit.SvgCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var document = XDocument.Load(@"c:\Users\mje\Desktop\HumanDesign.svg");
+
+            InkScapeSvgCleaner.Clean(document);
+
+            document.Save("HumanDesign.svg");
+
+            var content = File.ReadAllText("HumanDesign.svg");
+
+            var result = Regex.Replace(
+                content,
+                @"""#(?<name>[^#]+)#""", 
+                x => $"{{{UnEscape(x.Groups.Where(g => g.Name == "name").Single().Value)}}}");
+
+            File.WriteAllText("HumanDesign.svg", result);
+
+            File.WriteAllLines("HumanDesign.svg", File.ReadAllLines("HumanDesign.svg").Skip(2).ToArray());
+        }
+
+        private static string UnEscape(string value) =>
+            value.Replace("&gt;", ">");
+    }
+}
